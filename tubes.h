97801478c048b61fac9d8c4bd8582259a1eee8c7
himaana <himana.on@gmail.com>
2023-01-11 00:53:15 +0700

    add a function

--- tubes.h
+++ tubes.h
@@ -6,7 +6,6 @@ using namespace std;
 #define info(P) P->info
 #define next(P) P->next
 #define head(P) (P).head
-#define tail(P) (P).tail
 
 typedef struct Queue *adr;
 
@@ -24,7 +23,6 @@ struct Queue{
 
 struct List{
     adr head;
-    adr tail;
 };
 
 void createQueue(List &Q);
@@ -35,6 +33,7 @@ void show(List Q);
 void printJob(adr P);
 void AverageWT(float &x, int n);
 void AverageTAT(float &y, int n);
+void updateWTTAT(List &Q, adr temp, int quantum);
 List firstTimeFirstServe(List &Q1, List &Q2, List &Q3, int quantum, int prioritas, float &AWT, float &ATAT);
 bool isEmpty(List Q);
 int menu();
