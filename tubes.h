1f92416ab809a340fcfcdd1eeb2ebf6bbc5752c9
himaana <himana.on@gmail.com>
2023-01-10 11:35:22 +0700

    update dan menambahkan beberapa fungsi

---
+++ tubes.h
@@ -0,0 +1,42 @@
+#ifndef TUBESH_H_INCLUDED
+#define TUBESH_H_INCLUDED
+#include <iomanip>
+#include <iostream>
+using namespace std;
+#define info(P) P->info
+#define next(P) P->next
+#define head(P) (P).head
+#define tail(P) (P).tail
+
+typedef struct Queue *adr;
+
+struct infotype{
+    int burstTime;
+    int arroundTime;
+    int waitingTime;
+    string name;
+};
+
+struct Queue{
+    infotype info;
+    adr next;
+};
+
+struct List{
+    adr head;
+    adr tail;
+};
+
+void createQueue(List &Q);
+void createElement(infotype x, adr &P);
+void enqueue(List &Q, adr P);
+void dequeue(List &Q, adr &P);
+void show(List Q);
+void printJob(adr P);
+void AverageWT(float &x, int n);
+void AverageTAT(float &y, int n);
+List firstTimeFirstServe(List &Q1, List &Q2, List &Q3, int quantum, int prioritas, float &AWT, float &ATAT);
+bool isEmpty(List Q);
+int menu();
+
+#endif // TUBESH_H_INCLUDED
